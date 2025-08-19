307be8974105bbb4e95d32c5e00471bcaa13cbd6
eape3421 <eraldoeape@gmail.com>
2025-08-19 04:04:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -122,7 +122,7 @@ def comparar_metas(df_filtrado):
         st.info(f"🟡 Atenção: você está perto de ultrapassar a meta nas categorias: {', '.join(quase_estouradas)}")
 
 def exportar_e_enviar(df_filtrado, df):
-def grafico_pizza_alerta(categoria_total):
+    def grafico_pizza_alerta(categoria_total):
     st.subheader("🥧 Distribuição de gastos por categoria")
 
     metas = carregar_metas()
